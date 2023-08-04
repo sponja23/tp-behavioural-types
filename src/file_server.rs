0d666dfc6c67c41c66b56c4608dbec3d8c2857f2
sponja23 <tomas.spognardi@gmail.com>
2023-08-04 03:59:18 -0300

    Implementación parcial de FileServerWorker

--- src/file_server.rs
+++ src/file_server.rs
@@ -1,4 +1,8 @@
-use std::net::{TcpListener, ToSocketAddrs};
+use std::{
+    io::{BufRead, BufReader},
+    net::{TcpListener, TcpStream, ToSocketAddrs},
+    thread,
+};
 
 pub struct FileServer {
     socket: TcpListener,
@@ -6,7 +10,7 @@ pub struct FileServer {
 
 impl FileServer {
     pub fn new(address: impl ToSocketAddrs) -> FileServer {
-        let socket = TcpListener::bind(address).unwrap();
+        let socket = TcpListener::bind(address).expect("Failed to bind to port");
         FileServer { socket }
     }
 
@@ -15,9 +19,47 @@ impl FileServer {
             match stream {
                 Ok(stream) => {
                     println!("New connection: {}", stream.peer_addr().unwrap());
+                    thread::spawn(|| {
+                        let mut worker = FileServerWorker::new(stream);
+                        worker.start();
+                    });
                 }
                 Err(e) => {
                     println!("Error: {}", e);
+                }
+            }
+        }
+    }
+}
+
+pub struct FileServerWorker {
+    stream: BufReader<TcpStream>,
+}
+
+impl FileServerWorker {
+    pub fn new(stream: TcpStream) -> FileServerWorker {
+        FileServerWorker {
+            stream: BufReader::new(stream),
+        }
+    }
+
+    pub fn read_command(&mut self) -> String {
+        let mut buf = String::new();
+        self.stream
+            .read_line(&mut buf)
+            .expect("Failed to read from stream");
+        buf
+    }
+
+    pub fn start(&mut self) {
+        loop {
+            match self.read_command().trim() {
+                "REQUEST" => {
+                    todo!("handle request")
+                }
+                "CLOSE" => break,
+                _ => {
+                    println!("Invalid command")
                 }
             }
         }
